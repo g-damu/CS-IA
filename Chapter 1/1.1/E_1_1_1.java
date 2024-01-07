b72f29ff8f38e74526ecb645f37a78ffa4b41b3b
DakshinaMurthy Gudipalli <damu.g@live.com>
2024-01-07 23:42:49 +0530

    Create E_1_1_1.java

---
+++ Chapter 1/1.1/E_1_1_1.java
@@ -0,0 +1,14 @@
+public class E_1_1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
